d50c8f7e70d2c4addd287b87fb124d625bd596f2
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-05-11 10:31:12 +0200

    login now works

--- no-animations/script.js
+++ no-animations/script.js
@@ -1,18 +1,26 @@
 function hi() {
     console.log("hello!");
 }
-function validate() {
+async function validate() {
     hi();
     var x = document.forms["myForm"]["user"].value;
     var y = document.forms["myForm"]["pass"].value;
 
-    if (x == "" && y == "") {
+    if (x == "" || y == "") {
         hi();
         document.getElementById("centerContianer").style.background = "#ff5252";
         document.getElementById("centerContianer").style.boxShadow = "20px 20px 60px #850000, -20px -20px 60px #ff5252"; 
     }
     else {
+
         console.log("HAIIII");
+        //alert("haiiii! =^-^=");
+        await sleep(100);
         window.location.href="homepage.html"; 
     }
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
